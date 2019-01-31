9495271ee322fa7c2eb38483354ee95c3e5e56de
srose72 <sophia.rose7269@gmail.com>
2019-01-31 14:49:04 -0800

    Create GPSTests.ino
    Working code to fetch NMEA strings from Parallax GPS receiver

---
+++ GPSTests.ino
@@ -0,0 +1,20 @@
+#include <SoftwareSerial.h>
+
+//define receiver/transmitter pins
+#define rxPin 2
+#define txPin 3
+
+SoftwareSerial mySerial(rxPin, txPin); //declare SoftwareSerial object
+
+void setup() {
+  pinMode(rxPin, INPUT); //set receiver pin to input
+  Serial.begin(4800); //set serials to correct baud rate
+  mySerial.begin(4800);
+}
+
+void loop() {
+  if(mySerial.available()){ //if data is available
+    Serial.write(mySerial.read());  //print to Serial Monitor
+  }
+
+}
