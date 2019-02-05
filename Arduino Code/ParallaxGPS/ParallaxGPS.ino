99e777ceb8400bbb87b91ce06ccbea930258f673
srose72 <sophia.rose7269@gmail.com>
2019-02-04 19:54:32 -0800

    Update ParallaxGPS.ino
    Added comments. Unsure how to save the lat/lon as a float/double to convert to decimal degrees.

--- Arduino Code/ParallaxGPS/ParallaxGPS.ino
+++ Arduino Code/ParallaxGPS/ParallaxGPS.ino
@@ -12,10 +12,10 @@ void setup() {
   mySerial.begin(4800);
 }
 
-int commas = 0;
-bool valid = false;
-int lat[2] = {0,0};
-int lon[2] = {0,0};
+int commas = 0; //counting commas in NMEA sentence
+bool valid = false; //bool for navigation receiver warning
+int lat_ind[2] = {0,0}; //storing start/end indices for lat
+int lon_ind[2] = {0,0}; //storing start/end indices for lon
 
 void loop() {
 //  if(mySerial.available()){ //if data is available
@@ -23,13 +23,16 @@ void loop() {
 //  }
 
   char sentence[] = "$GPRMC,002105,A,3640.5557,N,12147.0016,W,000.2,082.1,010219,,,A*63";
-  
+
+  //Parses the NMEA sentence for nav receiver warning, lat, and lon
+  //If 2nd letter in NMEA code is 'R'...
   if(sentence[3] == 'R'){
-    for(int i = 5; i < sizeof(sentence); i++){
-      if(sentence[i] == ','){
+    for(int i = 5; commas <= 5; i++){ //only search sentence up to the 5th comma
+      if(sentence[i] == ','){ //count the commas as they are found
         commas++;  
       }
       else{
+        //Save nav receiver warning
         if(commas == 2){
           if(sentence[i] == 'A'){
 //            Serial.println(sentence[i]);
@@ -37,6 +40,7 @@ void loop() {
 //            Serial.println(valid);
           }
         }
+        //Save the starting index of latitude
         if(commas == 3){
 //          Serial.println(i);
           if(lat[0] == 0){
@@ -44,12 +48,20 @@ void loop() {
 //            Serial.println(lat[0]); 
           }
         }
+        //Save ending index of latitude and starting index of longitude
         if(commas == 4){
-          if(lat[1] == '0'){
-            lat[1] = (char)i;  
+          if(lat[1] == 0){
+            lat[1] = i-2;  
           }
-//          lon += sentence[i];  
-//          Serial.println(sentence[i]);
+          if(lon[0] == 0){
+            lon[0] = i; 
+          }
+        }
+        //Save ending index of longitude
+        if(commas == 5){
+          if(lon[1] == 0){
+            lon[1] = i-2;  
+          }  
         }
       }  
     }  
