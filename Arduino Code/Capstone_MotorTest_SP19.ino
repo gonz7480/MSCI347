eeb0e22b2bc44274feca796dbea79c929fec4bfa
gonz7480 <47131860+gonz7480@users.noreply.github.com>
2019-02-07 12:55:59 -0800

    Update Capstone_MotorTest_SP19.ino
    Updated commands and comments

--- Arduino Code/Capstone_MotorTest_SP19.ino
+++ Arduino Code/Capstone_MotorTest_SP19.ino
@@ -2,18 +2,21 @@
 Servo mtrwnch;
 
 void setup() {
-  mtrwnch.attach(5); 
+  mtrwnch.attach(5);                //attached to pin 5
 }
 
 void loop() {
-up();
+up();                               //indefinite movement on loop; need to add delay function
 }
 
-void stop(){
-mtrwnch.writeMicroseconds(1500);
-delay(500);
+void pause(){
+  mtrwnch.writeMicroseconds(1500);  //command to stop moving the motor
 }
 
 void up (){
-mtrwnch.writeMicroseconds(1200);
+  mtrwnch.writeMicroseconds(1200);  //command for motor to reel in
+}
+
+void down(){
+  mtrwnch.writeMicroseconds(1700);  //command for motor to reel back down
 }
