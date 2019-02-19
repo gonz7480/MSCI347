4195063ec7918b17d497bddb33b1b78afe1a140b
srose72 <sophia.rose7269@gmail.com>
2019-02-18 18:54:03 -0800

    Update Propellers.ino
    Code that was tested on 2/13/18

--- Arduino Code/Propellers/Propellers.ino
+++ Arduino Code/Propellers/Propellers.ino
@@ -1,19 +1,18 @@
 #include <Servo.h>
 
-#define MotorPin 8
+#define MotorPin 5
 
 Servo motor;
 
 void setup() {
   motor.attach(MotorPin);
 
+  //motors need a stop command before it can move
+  motor.writeMicroseconds(1500); 
+  delay(3000);
 }
 
 void loop() {
-  motor.writeMicroseconds(1550);
-  delay(2000);
-
-  motor.writeMicroseconds(1450);
-  delay(2000);
-
+  motor.writeMicroseconds(1650);
+  delay(3000);
 }
