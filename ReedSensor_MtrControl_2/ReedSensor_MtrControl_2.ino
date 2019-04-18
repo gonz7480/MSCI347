bb467ed8076708871ef70280d55c67d2105f0f52
srose72 <sophia.rose7269@gmail.com>
2019-04-17 21:37:20 -0700

    Updated ReedSensor_MtrControl_2.ino
    Added serial communication protocol for communicating with navigation Arduino

--- ReedSensor_MtrControl_2/ReedSensor_MtrControl_2.ino
+++ ReedSensor_MtrControl_2/ReedSensor_MtrControl_2.ino
@@ -4,10 +4,18 @@
 //           MSCI 437 Capstone       //
 //------------------------------------//
 #include <Servo.h>
+#include <SoftwareSerial.h>
 Servo WenchMtr; // Names the motor for later use in the program 
 const int pinSwitch = 12;  // Pin Reed set for pin 12 on Arduino 
 const int pinLed    = 9;  //Pin LED
 int StatoSwitch = 0;
+
+//KEEP THIS PINS CONSISTENT WITH NAVIGATION
+const byte RXNav = 7;
+const byte TXNav = 6;
+
+SoftwareSerial navigation (RXNav, TXNav);
+
 void up (){
    WenchMtr.writeMicroseconds(1200);
   ;
@@ -22,11 +30,22 @@ void pause (int time){
 }
 void setup()
 {
+  navigation.begin(9600);
   WenchMtr.attach(9);
   pinMode(pinSwitch, INPUT);
 }
+
 void loop()
 {
+  navigation.listen(); //listen for signal from GPS
+
+  //while no signal has been sent, stay in setup
+  while(!navigation.available()){ break; };
+
+  //NOT SURE IF WILL BE OKAY IF RECEIVING SIGNAL MULTIPLE TIMES.
+  //SOPHIA WILL THINK ON THIS PROBLEM FURTHER.
+  //SHE'S ALSO NOT SURE WHERE StatoSwitch LINE SHOULD GO
+  
   StatoSwitch = digitalRead(pinSwitch);  // Read the value given by the reed sensor
   if (StatoSwitch == HIGH) 
   {
@@ -35,5 +54,8 @@ void loop()
   else
   {
     up();
+
+    //Once observatory is up, send "home" signal to navigation
+    navigation.println('o');
   }
 }
