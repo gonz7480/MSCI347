de160a59492555b9b62a9dac378c172a2f2fd053
bear2297 <47126883+bear2297@users.noreply.github.com>
2019-05-01 20:33:37 -0700

    Update and rename Test_Options/DropPackage.ino to Test_Options/DropPackage/DropPackage.ino
    Added a simple delay timer

--- Test_Options/DropPackage/DropPackage.ino
+++ Test_Options/DropPackage/DropPackage.ino
@@ -2,42 +2,45 @@
     Written by Kaitlyn Beardshear
 */
 
-//Variables to change depth and time spent on bottom
-int depth = 2; //Measured in meters
-int bottomTime = 2; //Measured in seconds
+//Variables to change based on the launch
+int depth = 3; //How deep the Benthic Observatory is lowered (measured in meters)
+int bottomTime = 30; //How long the Benthic Observatory sits on the seafloor (measured in seconds)
+int delayStart = 5; //How long to wait before dropping the Benthic Observatory (measured in minutes)
 
 #include <Servo.h>  //Servo library
 
+//Defined pins and global variables
+//The reel switch is used to stop the winch
 int reelState; //Variable to hold if the switch is closed or not
-int reelSwitch = 41; //Pin connected to the reel switch
+#define reelSwitch 41 //Pin connected to the reel switch
 
-const int motorController = 30; //Winch controller
+//Winch variables
+#define motorController 30 //Winch controller
+int delayTime; //Variable to hold the start delay in ms
 int dropTime; //Variable to hold the droptime in ms
 int winchWait; //Variable to hold the bottom time in ms
-int i; //Variable to count what loop cycle winchDown in on
 int dropLoops; //The number of times the winchDown loop cycles
 
-//Declare servos
-Servo winchMtr; //Winch controller
+Servo winchMtr; //Declare Winch controller
 
-//function to pull  up the Benthic Observatory
+//Function to pull  up the Benthic Observatory
 void winchUp () {
   reelState = digitalRead(reelSwitch);
-  while (reelState == 1) { //if the magnet is not connected, keep pulling up
+  while (reelState == 1) { //If the switch is not connected, keep pulling up
     reelState = digitalRead(reelSwitch);
     winchMtr.writeMicroseconds(1200);
   }
   if (reelState == 0) {
-    winchStop(); //once the magnet is connected, stop
+    winchStop(); //Once the switch is connected, stop
   }
 }
 
 //Function to drop the Benthic Observatory
 void winchDown() {
-  for (i = 0; i < dropLoops; i++) { //Checks the reelSwitch every 200ms just in case the line becomes tangled while dropping
+  for (int i = 0; i < dropLoops; i++) { //Checks the reelSwitch every 200ms just in case the line becomes tangled while dropping
     reelState = digitalRead(reelSwitch);
-    if (reelState == 0) {
-      winchStop(); //Once the switch is connected, stop
+    if (reelState == 0) { //If the switch is connected, stop
+      winchStop(); 
     } else {
       winchMtr.writeMicroseconds(1700);
     }
@@ -50,9 +53,14 @@ void winchStop() {
   winchMtr.writeMicroseconds(1500);
 }
 
+//Function for the winch to hold the Benthic Observatory at the desired depth
 void winchPause(int winchWait) {
   winchMtr.writeMicroseconds(1500);
   delay(winchWait);
+}
+
+void startDelay(int delayTime) {
+  delay(delayTime);
 }
 
 void setup() {
@@ -60,10 +68,17 @@ void setup() {
   winchMtr.attach(30);  //Attach the motor 
   pinMode(reelSwitch, INPUT_PULLUP);  //Set the reelSwitch as an input
 
+  delayTime = ((delayStart) * 60000); //This changes the start delay from minutes to ms
   dropTime = ((depth) * 7000); //The winch takes approx 7 seconds to unspool 1 meter of string, this calculates
   // how long the winch needs to unspool based on the given depth.
   dropLoops = ((dropTime) / 200); //The loop needs to cycle every 200 ms for the total amount of time
   winchWait = ((bottomTime) * 1000); //This changes the bottom time from seconds to milliseconds
+
+//Current test
+startDelay(delayTime);
+winchDown();
+winchPause(winchWait);
+winchUp();
 }
 
 void loop() {
